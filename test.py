2b3b6745a46c7f5ee39d63a87eeb3e63b04ece9e
Andrew Kim <akim112@cs.brown.edu>
2019-10-28 20:46:28 -0400

    space filling

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from tronproblem import TronState
-from utils.heuristic import voronoi_heuristic
+from utils.heuristic import voronoi_heuristic, space_fill_heursitic
 
 board = [['#', '#', '#', '#', '#', '#'],
          ['#', '2', '1', ' ', ' ', '#'],
@@ -18,5 +18,6 @@ player_locs = [(1,2), (1, 1)]
 
 tron_st = TronState(board, player_locs, 1, {})
 
-print(voronoi_heuristic(tron_st))
+# print(voronoi_heuristic(tron_st))
+print(space_fill_heursitic(tron_st))
 
